3b22442bd94a36fd471584e6a77a1adc14268aa2
stephanbreimann <stephanbreimann@gmail.com>
2023-09-19 17:21:45 +0200

    Adjust conf.py/html_context to work online (not local)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -150,7 +150,7 @@ html_context = {
     'display_github': True,  # Add the 'Edit on GitHub' link
     'github_user': 'breimanntools',
     'github_repo': 'aaanalysis',
-    'github_version': 'master/docs/source/',
+    'github_version': 'master/',
 }
 
 
