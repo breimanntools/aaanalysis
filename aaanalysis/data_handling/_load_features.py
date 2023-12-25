8e20675ab8761bad64e5c1287478b9a5a5e2cba3
stephanbreimann <stephanbreimann@gmail.com>
2023-12-25 16:49:45 +0100

    Update load_features documentation and examples

--- aaanalysis/data_handling/_load_features.py
+++ aaanalysis/data_handling/_load_features.py
@@ -12,7 +12,7 @@ FOLDER_FEATURES = ut.FOLDER_DATA + "features" + ut.SEP
 
 # I Helper Functions
 def check_name(name=None):
-    """"""
+    """Check provided names of dataset"""
     if name not in LIST_DATASETS_WITH_FEATURES:
         raise ValueError(f"'name' should be one of the following: {LIST_DATASETS_WITH_FEATURES}")
 
@@ -26,14 +26,17 @@ def load_features(name="DOM_GSEC"):
 
     Parameters
     ----------
-    name
+    name : str, default='DOM_GSEC'
         The name of the dataset for which features are loaded.
 
     Returns
     -------
-    df_feat
+    df_feat : pd.DataFrame
         A DataFrame with features and their statistical measures as provided :meth:`aaanalysis.CPP.run` method,
         including their feature importance.
+
+    Examples
+    .. include:: examples/load_features.rst
     """
     # Check input
     check_name(name=name)
