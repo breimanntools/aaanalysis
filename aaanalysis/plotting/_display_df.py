f4c2e382da01f4cac6ad6cfb233ce281d3007d0a
stephanbreimann <stephanbreimann@gmail.com>
2024-01-09 16:58:51 +0100

    Update display_df and notebooks accordingly

--- aaanalysis/plotting/_display_df.py
+++ aaanalysis/plotting/_display_df.py
@@ -18,17 +18,51 @@ def _adjust_df(df=None, char_limit = 50):
         df = df.map(lambda x: f(x) if isinstance(x, str) and len(str(x)) > char_limit else x)
     return df
 
+def _check_show(name="row_to_show", val=None, df=None):
+    """Check if valid string or int"""
+    if val is None:
+        return None # Skip test
+    rows_or_columns = list(df.T) if "row" in name else list(df)
+    n = len(rows_or_columns)
+    str_row_or_column = "row" if "row" in name else "column"
+    if isinstance(val, str):
+        ut.check_str(name=name, val=val, accept_none=True)
+        if val not in rows_or_columns:
+            raise ValueError(f"'{name}' ('{val}') should be one of following: {rows_or_columns}")
+    elif isinstance(val, int):
+        ut.check_number_range(name=name, val=val, accept_none=True, min_val=0, max_val=n, just_int=True)
+    else:
+        raise ValueError(f"'{name}' ('{val}') should be int (<{n}) or one of following {str_row_or_column} names: {rows_or_columns}")
+
+def _select_row(df=None, row_to_show=None):
+    """Select row"""
+    if row_to_show is not None:
+        if isinstance(row_to_show, int):
+            df = df.iloc[[row_to_show]]
+        elif isinstance(row_to_show, str):
+            df = df.loc[[row_to_show]]
+    return df
+
+def _select_col(df=None, col_to_show=None):
+    """Select column"""
+    if col_to_show is not None:
+        if isinstance(col_to_show, int):
+            df = df.iloc[:, [col_to_show]]
+        elif isinstance(col_to_show, str):
+            df = df[[col_to_show]]
+    return df
+
 
 # Main functions
 def display_df(df: pd.DataFrame = None,
-               fontsize: Union[int, float] = 12,
                max_width_pct: int = 100,
                max_height: int = 300,
                char_limit: int = 30,
                show_shape=False,
                n_rows: Optional[int] = None,
                n_cols: Optional[int] = None,
-               n_round=3,
+               row_to_show: Optional[Union[int, str]] = None,
+               col_to_show: Optional[Union[int, str]] = None,
                ):
     """
     Display DataFrame with specific style as HTML output for jupyter notebooks.
@@ -37,42 +71,47 @@ def display_df(df: pd.DataFrame = None,
     ----------
     df : pd.DataFrame
         DataFrame to be displayed as HTML output.
-    fontsize : int, default=12
-        Relative font size in points of table font.
     max_width_pct: int, default=100
         Maximum width in percentage of main page for table.
     max_height : int, default=300
        Maximum height in pixels of table.
-    char_limit : int, default=25
+    char_limit : int, default=30
         Maximum number of characters to display in a cell.
     show_shape : bool, default=False
         If ``True``, shape of ``df`` is printed.
     n_rows : int, optional
-        Number of rows.
+        Display only the first n rows.
     n_cols : int, optional
-        Number of rows.
-    n_round : int, default=3
-        Rounding to a variable number of decimal places.
+        Display only the first n columns.
+    row_to_show : int or str, optional
+        Display only the specified row.
+    col_to_show : int or str, optional
+        Display only the specified column.
+
+    Examples
+    --------
+    .. include:: examples/display_df.rst
     """
     # Check input
     ut.check_df(name="df", df=df, accept_none=False)
-    ut.check_number_range(name="fontsize", val=fontsize, min_val=1, accept_none=False, just_int=True)
     ut.check_number_range(name="max_width_pct", val=max_width_pct, min_val=1, max_val=100, accept_none=False, just_int=True)
     ut.check_number_range(name="max_height", val=max_height, min_val=1, accept_none=False, just_int=True)
     ut.check_number_range(name="char_limit", val=char_limit, min_val=1, accept_none=True, just_int=True)
     ut.check_number_range(name="n_rows", val=n_rows, min_val=1, max_val=len(df), accept_none=True, just_int=True)
     ut.check_number_range(name="n_cols", val=n_cols, min_val=1, max_val=len(df.T), accept_none=True, just_int=True)
+    _check_show(name="show_only_col", val=col_to_show, df=df)
+    _check_show(name="show_only_row", val=row_to_show, df=df)
     # Show shape before filtering
     if show_shape:
         print(f"DataFrame shape: {df.shape}")
     # Filtering
     df = df.copy()
-    if n_rows is not None:
+    df = _select_col(df=df, col_to_show=col_to_show)
+    df = _select_row(df=df, row_to_show=row_to_show)
+    if row_to_show is None and n_rows is not None:
         df = df.head(n_rows)
-    if n_cols is not None:
+    if col_to_show is None and n_cols is not None:
         df = df.T.head(n_cols).T
-    if n_round is not None:
-        df = df.round(n_round)
     # Style dataframe
     df = _adjust_df(df=df, char_limit=char_limit)
     styled_df = (
@@ -86,8 +125,6 @@ def display_df(df: pd.DataFrame = None,
             {'selector': 'tbody tr:nth-child(even)', 'props': [('background-color', 'white')]},
             # General styling for table cells
             {'selector': 'th, td', 'props': [('padding', '5px'), ('white-space', 'nowrap')]},
-            # Font size for the table (does not work, overriden by CSS files)
-            {'selector': 'table', 'props': [('font-size', f'{fontsize}px')]},
         ])
     )
     display(HTML(styled_df.to_html()))
---
+++ tests/unit/plotting_tests/test_display_df.py
@@ -0,0 +1,126 @@
+"""
+This is a script for testing the aa.display_df function.
+"""
+from hypothesis import given, settings
+from hypothesis import strategies as st
+import pandas as pd
+import pytest
+import aaanalysis as aa
+
+@pytest.fixture(scope="module")
+def sample_dataframe():
+    return pd.DataFrame({
+        'A': [1, 2, 3],
+        'B': ['a', 'b', 'c']
+    })
+
+class TestDisplayDf:
+    """Test display_df function for individual parameters"""
+
+    def test_valid_dataframe(self):
+        # Test with valid DataFrame
+        df = aa.load_scales()
+        aa.display_df(df)
+
+    @settings(max_examples=10)
+    @given(max_width_pct=st.integers(min_value=1, max_value=100))
+    def test_max_width_pct(self, max_width_pct, sample_dataframe):
+        aa.display_df(sample_dataframe, max_width_pct=max_width_pct)
+
+    @settings(max_examples=10)
+    @given(max_height=st.integers(min_value=1))
+    def test_max_height(self, max_height, sample_dataframe):
+        aa.display_df(sample_dataframe, max_height=max_height)
+
+    @settings(max_examples=10)
+    @given(char_limit=st.integers(min_value=1))
+    def test_char_limit(self, char_limit, sample_dataframe):
+        aa.display_df(sample_dataframe, char_limit=char_limit)
+
+    @settings(max_examples=10)
+    @given(n_rows=st.integers(min_value=1))
+    def test_n_rows(self, n_rows, sample_dataframe):
+        if n_rows < len(sample_dataframe):
+            aa.display_df(sample_dataframe, n_rows=n_rows)
+
+    @settings(max_examples=10)
+    @given(n_cols=st.integers(min_value=1))
+    def test_n_cols(self, n_cols, sample_dataframe):
+        if n_cols < len(list(sample_dataframe)):
+            aa.display_df(sample_dataframe, n_cols=n_cols)
+
+    def test_row_to_show(self, sample_dataframe):
+        for row in list(sample_dataframe.T):
+            aa.display_df(sample_dataframe, row_to_show=row)
+
+    def test_col_to_show(self, sample_dataframe):
+        for col in list(sample_dataframe):
+            aa.display_df(sample_dataframe, col_to_show=col)
+
+    def test_show_shape(self, sample_dataframe):
+        for show_shape in [True, False]:
+            aa.display_df(sample_dataframe, show_shape=show_shape)
+
+    # Negative Tests
+    @pytest.mark.parametrize("invalid_value", [-1, 0, 101, 'invalid'])
+    def test_invalid_max_width_pct(self, invalid_value, sample_dataframe):
+        with pytest.raises(ValueError):
+            aa.display_df(sample_dataframe, max_width_pct=invalid_value)
+
+    @pytest.mark.parametrize("invalid_value", [-1, 0, 'invalid'])
+    def test_invalid_max_height(self, invalid_value, sample_dataframe):
+        with pytest.raises(ValueError):
+            aa.display_df(sample_dataframe, max_height=invalid_value)
+
+    @pytest.mark.parametrize("invalid_value", [-1, 0, 'invalid'])
+    def test_invalid_char_limit(self, invalid_value, sample_dataframe):
+        with pytest.raises(ValueError):
+            aa.display_df(sample_dataframe, char_limit=invalid_value)
+
+    @pytest.mark.parametrize("invalid_value", [-1, 0, 'invalid'])
+    def test_invalid_n_rows(self, invalid_value, sample_dataframe):
+        with pytest.raises(ValueError):
+            aa.display_df(sample_dataframe, n_rows=invalid_value)
+
+    @pytest.mark.parametrize("invalid_value", [-1, 0, 'invalid'])
+    def test_invalid_n_cols(self, invalid_value, sample_dataframe):
+        with pytest.raises(ValueError):
+            aa.display_df(sample_dataframe, n_cols=invalid_value)
+
+    @pytest.mark.parametrize("invalid_value", [-1, "0", 5.5])
+    def test_invalid_row_to_show(self, invalid_value, sample_dataframe):
+        with pytest.raises(ValueError):
+            aa.display_df(sample_dataframe, row_to_show=invalid_value)
+
+    @pytest.mark.parametrize("invalid_value", [-1, "0", 5.5])
+    def test_invalid_col_to_show(self, invalid_value, sample_dataframe):
+        with pytest.raises(ValueError):
+            aa.display_df(sample_dataframe, col_to_show=invalid_value)
+
+class TestDisplayDfComplex:
+    """Test display_df function with complex scenarios"""
+
+    @settings(max_examples=5)
+    @given(
+        max_width_pct=st.integers(min_value=1, max_value=100),
+        max_height=st.integers(min_value=1),
+        char_limit=st.integers(min_value=1),
+        show_shape=st.booleans(),
+        n_rows=st.integers(min_value=1),
+        n_cols=st.integers(min_value=1),
+    )
+    def test_valid_combinations(self, max_width_pct, max_height, char_limit, show_shape, n_rows, n_cols, sample_dataframe):
+        if n_cols < len(list(sample_dataframe)) and n_rows < len(sample_dataframe):
+            aa.display_df(sample_dataframe, max_width_pct, max_height, char_limit, show_shape, n_rows, n_cols)
+
+    @settings(max_examples=5)
+    @given(
+        max_width_pct=st.integers(min_value=101),
+        max_height=st.integers(max_value=0),
+        char_limit=st.integers(max_value=0),
+        n_rows=st.integers(max_value=0),
+        n_cols=st.integers(max_value=0)
+    )
+    def test_invalid_combinations(self, max_width_pct, max_height, char_limit, n_rows, n_cols, sample_dataframe):
+        with pytest.raises(ValueError):
+            aa.display_df(sample_dataframe, max_width_pct, max_height, char_limit, n_rows=n_rows, n_cols=n_cols)
