f2527b056fa896704ead1783b4bacfa2d9aa2161
stephanbreimann <stephanbreimann@gmail.com>
2024-04-05 19:25:10 +0200

    Update requirements.txt

--- noxfile.py
+++
@@ -1,7 +0,0 @@
-import nox
-
-@nox.session(python=["3.9", "3.10", "3.11", "3.12"])
-def tests(session):
-    session.install("-r", "requirements.txt")
-    session.install("pytest")
-    session.run("pytest", "--verbose", "tests/")
