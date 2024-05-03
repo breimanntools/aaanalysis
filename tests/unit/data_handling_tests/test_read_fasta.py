51bd7eee267ad643b91deafe4ff60c8600e0526e
stephanbreimann <stephanbreimann@gmail.com>
2024-05-03 14:09:08 +0200

    Update tests for read_fasta

--- tests/unit/data_handling_tests/test_read_fasta.py
+++ tests/unit/data_handling_tests/test_read_fasta.py
@@ -10,6 +10,15 @@ settings.load_profile("ci")
 
 FILE_IN = "valid_path.fasta"
 FILE_DB_IN = "valid_path_db.fasta"
+COL_DB = "database"
+
+def creat_mock_file():
+    """"""
+    df_seq = aa.load_dataset(name="SEQ_AMYLO", n=10)
+    df_seq["pred"] = range(len(df_seq))
+    aa.to_fasta(df_seq=df_seq, file_path=FILE_IN, cols_info=["pred"])
+    df_seq[COL_DB] = "sp"
+    aa.to_fasta(df_seq=df_seq, file_path=FILE_DB_IN, cols_info=["pred"], col_db=COL_DB)
 
 
 class TestReadFasta:
@@ -18,40 +27,44 @@ class TestReadFasta:
     # Property-based testing for positive cases
     def test_file_path(self):
         """Test the 'file_path' parameter with valid and invalid paths."""
+        creat_mock_file()
         df = aa.read_fasta(file_path=FILE_IN)
         assert isinstance(df, pd.DataFrame)  # Expecting a DataFrame to be returned
 
     @given(col_id=st.text(min_size=1))
     def test_col_id(self, col_id):
         """Test valid 'col_id' parameter."""
+        creat_mock_file()
         df = aa.read_fasta(file_path=FILE_IN, col_id=col_id)
         assert col_id in df.columns
 
     @given(col_seq=st.text(min_size=1))
     def test_col_seq(self, col_seq):
         """Test valid 'col_seq' parameter."""
+        creat_mock_file()
         df = aa.read_fasta(file_path=FILE_IN, col_seq=col_seq)
         assert col_seq in df.columns
 
     @given(cols_info=st.lists(st.text(min_size=1), min_size=1, max_size=1))
     def test_cols_info(self, cols_info):
         """Test valid 'cols_info' parameter."""
+        creat_mock_file()
         df = aa.read_fasta(file_path=FILE_IN, cols_info=cols_info)
         for col in cols_info:
             assert col in df.columns
 
-    @given(col_db=st.one_of(st.none(), st.text(min_size=1)))
-    def test_col_db(self, col_db):
+    def test_col_db(self):
         """Test valid 'col_db' parameter."""
+        creat_mock_file()
         with warnings.catch_warnings():
             warnings.simplefilter("ignore", category=UserWarning)
-            df = aa.read_fasta(file_path=FILE_DB_IN, col_db=col_db)
-            if col_db:
-                assert col_db in df.columns
+            df = aa.read_fasta(file_path=FILE_DB_IN, col_db=COL_DB)
+            assert COL_DB in df.columns
 
     @given(sep=st.text(min_size=1, max_size=1))
     def test_sep(self, sep):
         """Test valid 'sep' parameter."""
+        creat_mock_file()
         with warnings.catch_warnings():
             warnings.simplefilter("ignore", category=UserWarning)
             df = aa.read_fasta(file_path=FILE_IN, sep=sep)
@@ -60,24 +73,28 @@ class TestReadFasta:
     # Property-based testing for negative cases
     def test_invalid_col_id(self):
         """Test invalid 'col_id' parameter."""
+        creat_mock_file()
         for col_id in [None, [], {}, 34]:
             with pytest.raises(ValueError):
                 aa.read_fasta(file_path=FILE_IN, col_id=col_id)
 
     def test_invalid_col_seq(self):
         """Test invalid 'col_seq' parameter."""
+        creat_mock_file()
         for col_seq in [None, [], {}, 34]:
             with pytest.raises(ValueError):
                 aa.read_fasta(file_path=FILE_IN, col_seq=col_seq)
 
     def test_invalid_col_db(self):
         """Test invalid 'col_db' parameter."""
+        creat_mock_file()
         for col_db in [[], {}, 34]:
             with pytest.raises(ValueError):
                 aa.read_fasta(file_path=FILE_DB_IN, col_db=col_db)
 
     def test_invalid_cols_info(self):
         """Test invalid 'cols_info' parameter."""
+        creat_mock_file()
         with pytest.raises(ValueError):
             aa.read_fasta(file_path=FILE_IN, cols_info={})
         with pytest.raises(ValueError):
@@ -87,6 +104,7 @@ class TestReadFasta:
 
     def test_invalid_sep(self):
         """Test invalid 'sep' parameter."""
+        creat_mock_file()
         for sep in [[], {}, 34]:
             with pytest.raises(ValueError):
                 aa.read_fasta(file_path=FILE_IN, sep=sep)
@@ -98,6 +116,7 @@ class TestReadFastaComplex:
     @given(col_id=st.text(min_size=1), col_seq=st.text(min_size=1), sep=st.text(min_size=1, max_size=1))
     def test_combination_valid_inputs(self, col_id, col_seq, sep):
         """Test valid combinations of parameters."""
+        creat_mock_file()
         try:
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore", category=UserWarning)
@@ -111,6 +130,7 @@ class TestReadFastaComplex:
     @given(col_id=st.text(max_size=0), col_seq=st.text(min_size=1), sep=st.integers())
     def test_combination_invalid(self, col_id, col_seq, sep):
         """Test invalid 'col_id' in combination with other parameters."""
+        creat_mock_file()
         with pytest.raises(ValueError):
             aa.read_fasta(file_path=FILE_IN, col_id=col_id, col_seq=col_seq, sep=sep)
 
