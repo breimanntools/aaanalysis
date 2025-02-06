bc7e1f369579bd4f48e11008e350037b70ed238d
stephanbreimann <stephanbreimann@gmail.com>
2025-02-06 16:36:13 +0100

    Improve efficiency of add_stat for cpp.run()

--- aaanalysis/feature_engineering/_backend/cpp/_utils_feature_stat.py
+++ aaanalysis/feature_engineering/_backend/cpp/_utils_feature_stat.py
@@ -10,6 +10,7 @@ import aaanalysis.utils as ut
 # I Helper Functions
 # Benjamini Hochberg correction
 def _bh_corrected_pvalues(pvals):
+    """BH correction"""
     pvals = np.array(pvals)
     n = len(pvals)
     sorted_indices = np.argsort(pvals)
@@ -23,9 +24,29 @@ def _bh_corrected_pvalues(pvals):
     return corrected_pvals_original_order
 
 
+def _mann_whitney_u_vectorized(X, mask_test, mask_ref):
+    """Vectorized Mann-Whitney U Test Without Loops, same results as stats.mannwhitneyu (alternative="two-sided")"""
+    # Extract test & reference groups
+    X_test, X_ref = X[mask_test], X[mask_ref]
+    # Rank all values together (vectorized)
+    combined = np.concatenate([X_test, X_ref], axis=0)
+    ranks = stats.rankdata(combined, axis=0)
+    # Compute U-statistic directly (vectorized)
+    n1, n2 = X_test.shape[0], X_ref.shape[0]
+    U1 = np.sum(ranks[:n1], axis=0) - (n1 * (n1 + 1)) / 2
+    U2 = np.sum(ranks[n1:], axis=0) - (n2 * (n2 + 1)) / 2
+    U = np.minimum(U1, U2)  # Use smaller U for two-sided test
+    # Compute p-values using normal approximation
+    mean_U = n1 * n2 / 2
+    std_U = np.sqrt(n1 * n2 * (n1 + n2 + 1) / 12)
+    z = (U - mean_U) / std_U
+    p_vals = 2 * stats.norm.sf(np.abs(z))  # Two-sided p-value
+    return p_vals
+
+
 # Summary and test statistics for feature matrix based on classification by labels
 def _mean_dif(X=None, labels=None, label_test=1, label_ref=0):
-    """ Get mean difference for values in X (feature matrix) based on y (labels)"""
+    """Get mean difference for values in X (feature matrix) based on y (labels)"""
     mask_test = [x == label_test for x in labels]
     mask_ref = [x == label_ref for x in labels]
     mean_difs = np.mean(X[mask_test], axis=0) - np.mean(X[mask_ref], axis=0)
@@ -56,7 +77,7 @@ def _p_correction(p_vals=None):
     return p_corrected
 
 
-def _mean_stat(X=None, labels=None, parametric=False, p_cor=False, label_test=1, label_ref=0):
+def _mean_stat(X=None, labels=None, parametric=False, label_test=1, label_ref=0):
     """Statistical comparison of central tendency between two groups for each feature"""
     mask_test = [x == label_test for x in labels]
     mask_ref = [x == label_ref for x in labels]
@@ -64,13 +85,8 @@ def _mean_stat(X=None, labels=None, parametric=False, p_cor=False, label_test=1,
         p_vals = stats.ttest_ind(X[mask_test], X[mask_ref], nan_policy="omit")[1]
         p_str = "p_val_ttest_indep"
     else:
-        t = lambda x1, x2: stats.mannwhitneyu(x1, x2, alternative="two-sided")[1]  # Test statistic
-        c = lambda x1, x2: np.mean(x1) != np.mean(x2) or np.std(x1) != np.std(x2)  # Test condition
-        p_vals = np.round([t(col[mask_test], col[mask_ref]) if c(col[mask_test], col[mask_ref]) else 1 for col in X.T], 10)
+        p_vals = _mann_whitney_u_vectorized(X, mask_test, mask_ref)
         p_str = "p_val_mann_whitney"
-    if p_cor:
-        p_vals = _p_correction(p_vals=p_vals)
-        p_str = "p_val_fdr_bh"
     return p_vals, p_str
 
 
@@ -86,10 +102,11 @@ def add_stat_(df=None, X=None, labels=None, parametric=False, label_test=1, labe
         df[ut.COL_ABS_MEAN_DIF] = abs(_mean_dif(X=X, **args_labels))
     df[ut.COL_STD_TEST] = _std(X=X, labels=labels, group=label_test)
     df[ut.COL_STD_REF] = _std(X=X, labels=labels, group=label_ref)
-    p_val, p_str = _mean_stat(X=X, parametric=parametric, **args_labels)
-    df[p_str] = p_val
-    p_val_fdr, p_str_fdr = _mean_stat(X=X, parametric=parametric, p_cor=True, **args_labels)
-    df[p_str_fdr] = p_val_fdr
+    p_vals, p_str = _mean_stat(X=X, parametric=parametric, **args_labels)
+    df[p_str] = p_vals
+    p_vals_fdr = _p_correction(p_vals=p_vals)
+    p_str_fdr = "p_val_fdr_bh"
+    df[p_str_fdr] = p_vals_fdr
     cols_stat = [ut.COL_ABS_AUC,
                  ut.COL_ABS_MEAN_DIF, ut.COL_MEAN_DIF,
                  ut.COL_STD_TEST, ut.COL_STD_REF,
