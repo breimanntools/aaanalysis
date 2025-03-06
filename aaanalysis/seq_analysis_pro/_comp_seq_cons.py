9ca8e2d0583f657e795c30c376c04b73089d496f
stephanbreimann <stephanbreimann@gmail.com>
2025-03-06 20:11:18 +0100

    Add new module for computing sequence conservation

---
+++ aaanalysis/seq_analysis_pro/_comp_seq_cons.py
@@ -0,0 +1,38 @@
+"""
+This is a script for ...
+"""
+import time
+import pandas as pd
+import numpy as np
+
+
+# Settings
+pd.set_option('expand_frame_repr', False)  # Single line print for pd.Dataframe
+
+
+# I Helper Functions
+
+# TODO compute sequence conservation per position (input df_seq or df_parts)
+# TODO interface to biopython (use different tools to get MSA, MSA for conersvation)
+# II Main Functions
+def get_msa():
+    """"""
+
+
+def comp_seq_cons():
+    """"""
+
+
+# III Test/Caller Functions
+
+
+# IV Main
+def main():
+    t0 = time.time()
+
+    t1 = time.time()
+    print("Time:", t1 - t0)
+
+
+if __name__ == "__main__":
+    main()
