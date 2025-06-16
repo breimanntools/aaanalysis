807c1da969339019742591f61e6d9f93a83a1d42
stephanbreimann <stephanbreimann@gmail.com>
2025-06-16 22:22:58 +0200

    Update __init__.py to ensure (a) dynamic importing and (b) IDE support (go to definition)

--- aaanalysis/__init__.py
+++ aaanalysis/__init__.py
@@ -53,7 +53,7 @@ __all__ = [
 
 # Dynamically import professional (pro) features if dependencies are available
 def raise_error(feature_name=None, error_message=None):
-    """Dynamically set the fallback for missing features and raise ImportError"""
+    """Raise an informative ImportError with optional instructions for missing pro features."""
     str_error_message = str(error_message)
     if "No module named" in str_error_message:
         str_error = (f"'{feature_name}' needs additional dependencies. Install AAanalysis Professional via: "
@@ -63,12 +63,22 @@ def raise_error(feature_name=None, error_message=None):
         raise ImportError(error_message)
 
 
+def missing_feature_stub(feature_name, error):
+    """Return a callable that raises an ImportError when used
+
+    This acts as a **stub** — a placeholder function that represents a missing feature.
+    Instead of breaking the import, the stub delays the error until the user tries to use
+    the unavailable feature (e.g., `ShapModel()`), providing a clear message on how to fix it
+    """
+    return lambda *args, **kwargs: raise_error(feature_name=feature_name, error_message=error)
+
+
 try:
     from .explainable_ai_pro import ShapModel
     __all__.append("ShapModel")
 except ImportError as e:
-    ShapModel = None  # For IDE navigation
-    globals()["ShapModel"] = raise_error(feature_name="ShapModel", error_message=e)
+    ShapModel = None
+    globals()["ShapModel"] = missing_feature_stub("ShapModel", e)
 
 
 try:
@@ -76,19 +86,20 @@ try:
     __all__.append("comp_seq_sim")
 except ImportError as e:
     comp_seq_sim = None
-    globals()["comp_seq_sim"] = raise_error(feature_name="comp_seq_sim", error_message=e)
+    globals()["comp_seq_sim"] = missing_feature_stub("comp_seq_sim", e)
 
 
 try:
     from .seq_analysis_pro import filter_seq
     __all__.append("filter_seq")
 except ImportError as e:
-    comp_seq_sim = None
-    globals()["filter_seq"] = raise_error(feature_name="filter_seq", error_message=e)
+    filter_seq = None
+    globals()["filter_seq"] = missing_feature_stub("filter_seq", e)
+
 
 try:
     from .show_html import display_df
     __all__.append("display_df")
 except ImportError as e:
-    comp_seq_sim = None
-    globals()["display_df"] = raise_error(feature_name="display_df", error_message=e)
+    display_df = None
+    globals()["display_df"] = missing_feature_stub("display_df", e)
