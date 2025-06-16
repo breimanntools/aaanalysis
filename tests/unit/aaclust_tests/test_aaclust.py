4ae9a53f5dd1cb929649cbebee71b50a4af6e629
stephanbreimann <stephanbreimann@gmail.com>
2025-06-16 23:14:27 +0200

    Update tests

--- tests/unit/aaclust_tests/test_aaclust.py
+++ tests/unit/aaclust_tests/test_aaclust.py
@@ -37,7 +37,7 @@ X_mock = [[0.1, 0.2], [0.3, 0.4], [0.5, 0.6]]  # Mocked data
 class TestAAclust:
     """Test aa.AAclust class individual parameters"""
 
-    @settings(deadline=1000)
+    @settings(deadline=2000)
     @given(model_class_name=some.none() | some.sampled_from(list(K_BASED_MODELS.keys())))
     def test_k_based_model_parameter(self, model_class_name):
         """Test the 'model' parameter for k_based models."""
@@ -52,7 +52,7 @@ class TestAAclust:
             with pytest.raises(ValueError):
                 aa.AAclust(model_class=model_class_name)
 
-    @settings(deadline=1000)
+    @settings(deadline=2000)
     @given(model_class_name=some.sampled_from(list(K_BASED_MODELS.keys())))
     def test_k_based_model_parameter_after_fit(self, model_class_name):
         """Test the 'model' parameter for k_based models after fitting."""
@@ -61,14 +61,14 @@ class TestAAclust:
         aaclust.fit(X_mock)
         assert isinstance(aaclust.model, ModelClass)
 
-    @settings(deadline=1000)
+    @settings(deadline=2000)
     @given(model_class_name=some.sampled_from(list(K_FREE_MODELS.keys())))
     def test_k_free_model_parameter_negative(self, model_class_name):
         """Negative test for 'model' parameter for k_free models."""
         with pytest.raises(ValueError):
             aa.AAclust(model_class=model_class_name)
 
-    @settings(deadline=1000, max_examples=10)
+    @settings(deadline=2000, max_examples=10)
     @given(model_kwargs=some.dictionaries(keys=some.text(), values=some.integers()))
     def test_model_kwargs_parameter(self, model_kwargs):
         """Test the 'model_kwargs' parameter."""
@@ -85,15 +85,15 @@ class TestAAclust:
         """Test the 'verbose' parameter."""
         aa.options["verbose"] = "off"
         aaclust = aa.AAclust(verbose=True)
-        assert aaclust._verbose == True
+        assert aaclust._verbose is True
         aaclust = aa.AAclust(verbose=False)
-        assert aaclust._verbose == False
+        assert aaclust._verbose is False
 
 
 class TestAAclustComplex:
     """Test aa.AAclust class with complex scenarios"""
 
-    @settings(deadline=1000, max_examples=10)
+    @settings(deadline=2000, max_examples=10)
     @given(model_class_name=some.none() | some.sampled_from(list(K_BASED_MODELS.keys())),
            model_kwargs=some.dictionaries(keys=some.text(), values=some.integers()))
     def test_k_based_model_and_model_kwargs(self, model_class_name, model_kwargs):
@@ -113,7 +113,7 @@ class TestAAclustComplex:
             with pytest.raises(ValueError):
                 aa.AAclust(model_class=model_class_name, model_kwargs=model_kwargs)
 
-    @settings(deadline=1000, max_examples=10)
+    @settings(deadline=2000, max_examples=10)
     @given(model_kwargs=some.dictionaries(keys=some.text(), values=some.integers()),
            verbose=some.booleans())
     def test_model_kwargs_and_verbose(self, model_kwargs, verbose):
