53b612b8490ecf2d9f31c6e81364a763928c939c
Stephan Breimann <stephanbreimann@gmail.com>
2026-04-06 19:39:23 +0200

    Increase deadline for cpp.feature_map()

--- tests/unit/cpp_plot_tests/test_cpp_plot_feature_map.py
+++ tests/unit/cpp_plot_tests/test_cpp_plot_feature_map.py
@@ -103,7 +103,7 @@ class TestCCPlotFeatureMap:
             assert isinstance(fig, plt.Figure) and isinstance(ax, plt.Axes)
             plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(figsize=st.tuples(st.floats(min_value=4.0, max_value=20.0), st.floats(min_value=5.0, max_value=20.0)))
     def test_figsize(self, figsize):
         cpp_plot = aa.CPPPlot()
@@ -113,7 +113,7 @@ class TestCCPlotFeatureMap:
         plt.close()
 
     # Positive tests: Appearance of Parts (TMD-JMD)
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(start=st.integers(min_value=0, max_value=1000))
     def test_start(self, start):
         if start <= 1000:
@@ -123,7 +123,7 @@ class TestCCPlotFeatureMap:
             assert isinstance(fig, plt.Figure) and isinstance(ax, plt.Axes)
             plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(tmd_len=st.integers(min_value=20, max_value=100))
     def test_tmd_len(self, tmd_len):
         if tmd_len <= 1000:
@@ -141,7 +141,7 @@ class TestCCPlotFeatureMap:
         assert isinstance(fig, plt.Figure) and isinstance(ax, plt.Axes)
         plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(tmd_color=st.sampled_from(VALID_COLORS), jmd_color=st.sampled_from(VALID_COLORS),
            tmd_seq_color=st.sampled_from(VALID_COLORS), jmd_seq_color=st.sampled_from(VALID_COLORS))
     def test_color_parameters(self, tmd_color, jmd_color, tmd_seq_color, jmd_seq_color):
@@ -152,7 +152,7 @@ class TestCCPlotFeatureMap:
         assert isinstance(fig, plt.Figure) and isinstance(ax, plt.Axes)
         plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(seq_size=st.floats(min_value=8.0, max_value=14.0),
            fontsize=st.floats(min_value=8.0, max_value=14.0))
     def test_font_sizes(self, seq_size, fontsize):
@@ -167,7 +167,7 @@ class TestCCPlotFeatureMap:
         assert isinstance(fig, plt.Figure) and isinstance(ax, plt.Axes)
         plt.close()
 
-    @settings(max_examples=10, deadline=5000)
+    @settings(max_examples=10, deadline=10000)
     @given(weight_tmd_jmd=st.sampled_from(VALID_WEIGHT))
     def test_weight_tmd_jmd(self, weight_tmd_jmd):
         cpp_plot = aa.CPPPlot()
@@ -185,7 +185,7 @@ class TestCCPlotFeatureMap:
             plt.close()
 
     # Positive tests: Legend, Axis, and Grid Configurations
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(linewidth=st.floats(min_value=0.0, max_value=10.0))
     def test_linewidth(self, linewidth):
         cpp_plot = aa.CPPPlot()
@@ -211,7 +211,7 @@ class TestCCPlotFeatureMap:
             assert isinstance(fig, plt.Figure) and isinstance(ax, plt.Axes)
             plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(vmin=st.one_of(st.none(), st.integers(min_value=0), st.floats(min_value=0.0)),
            vmax=st.one_of(st.none(), st.integers(min_value=1), st.floats(min_value=1.0)))
     def test_vmin_vmax(self, vmin, vmax):
@@ -223,7 +223,7 @@ class TestCCPlotFeatureMap:
             assert isinstance(fig, plt.Figure) and isinstance(ax, plt.Axes)
             plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(cmap=st.one_of(st.none(), st.sampled_from(['viridis', 'plasma', 'inferno', 'magma', 'cividis'])))
     def test_cmap(self, cmap):
         cpp_plot = aa.CPPPlot()
@@ -232,7 +232,7 @@ class TestCCPlotFeatureMap:
         assert isinstance(fig, plt.Figure) and isinstance(ax, plt.Axes)
         plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(cmap_n_colors=st.integers(min_value=2, max_value=200))
     def test_cmap_n_colors(self, cmap_n_colors):
         cpp_plot = aa.CPPPlot()
@@ -241,7 +241,7 @@ class TestCCPlotFeatureMap:
         assert isinstance(fig, plt.Figure) and isinstance(ax, plt.Axes)
         plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(cbar_pct=st.booleans())
     def test_cbar_pct(self, cbar_pct):
         cpp_plot = aa.CPPPlot()
@@ -258,7 +258,7 @@ class TestCCPlotFeatureMap:
         assert isinstance(fig, plt.Figure) and isinstance(ax, plt.Axes)
         plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(cbar_xywh=st.tuples(st.floats(min_value=0.0, max_value=1.0),
                                st.one_of(st.none(), st.floats(min_value=0.0, max_value=1.0)),
                                st.floats(min_value=0.0, max_value=1.0),
@@ -285,7 +285,7 @@ class TestCCPlotFeatureMap:
         assert isinstance(fig, plt.Figure) and isinstance(ax, plt.Axes)
         plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(legend_xy=st.tuples(st.floats(min_value=-1.0, max_value=1.0), st.floats(min_value=-1.0, max_value=1.0)))
     def test_legend_xy(self, legend_xy):
         cpp_plot = aa.CPPPlot()
@@ -345,7 +345,7 @@ class TestCCPlotFeatureMap:
             assert isinstance(fig, plt.Figure) and isinstance(ax, plt.Axes)
             plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(xtick_size=st.floats(min_value=8.0, max_value=14.0), xtick_width=st.floats(min_value=0.5, max_value=2.0),
            xtick_length=st.floats(min_value=3.0, max_value=10.0))
     def test_x_tick_styling(self, xtick_size, xtick_width, xtick_length):
@@ -372,7 +372,7 @@ class TestCCPlotFeatureMap:
             cpp_plot.feature_map(df_feat=df_feat)
         plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(col_cat=st.sampled_from(INVALID_COL_CATS))
     def test_invalid_col_cat(self, col_cat):
         cpp_plot = aa.CPPPlot()
@@ -381,7 +381,7 @@ class TestCCPlotFeatureMap:
             cpp_plot.feature_map(df_feat=df_feat, col_cat=col_cat)
         plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(col_val=st.sampled_from(INVALID_COL_VALS))
     def test_invalid_col_val(self, col_val):
         cpp_plot = aa.CPPPlot()
@@ -408,7 +408,7 @@ class TestCCPlotFeatureMap:
                 cpp_plot.feature_map(df_feat=df_feat, name_test=valid_names, name_ref=valid_names)
             plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(figsize=st.tuples(st.just(-10.0), st.just(-10.0)))
     def test_invalid_figsize(self, figsize):
         cpp_plot = aa.CPPPlot()
@@ -426,7 +426,7 @@ class TestCCPlotFeatureMap:
             plt.close()
 
     # Negative tests: Appearance of Parts (TMD-JMD)
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(start=st.integers(max_value=-1))  # Invalid negative start
     def test_invalid_start(self, start):
         cpp_plot = aa.CPPPlot()
@@ -435,7 +435,7 @@ class TestCCPlotFeatureMap:
             cpp_plot.feature_map(df_feat=df_feat, start=start)
         plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(tmd_len=st.integers(max_value=0))  # Invalid non-positive tmd_len
     def test_invalid_tmd_len(self, tmd_len):
         cpp_plot = aa.CPPPlot()
@@ -444,7 +444,7 @@ class TestCCPlotFeatureMap:
             cpp_plot.feature_map(df_feat=df_feat, tmd_len=tmd_len)
         plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(tmd_seq=st.text(), jmd_n_seq=st.text(), jmd_c_seq=st.text())
     def test_invalid_sequences(self, tmd_seq, jmd_n_seq, jmd_c_seq):
         if not isinstance(tmd_seq, str) or not isinstance(jmd_n_seq, str) or not isinstance(jmd_c_seq, str):
@@ -454,7 +454,7 @@ class TestCCPlotFeatureMap:
                 cpp_plot.feature_map(df_feat=df_feat, tmd_seq=tmd_seq, jmd_n_seq=jmd_n_seq, jmd_c_seq=jmd_c_seq)
             plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(color=st.sampled_from(INVALID_COLORS))
     def test_invalid_color_parameters(self, color):
         cpp_plot = aa.CPPPlot()
@@ -465,7 +465,7 @@ class TestCCPlotFeatureMap:
         plt.close()
 
     # Negative tests for Appearance of Parts (TMD-JMD)
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(seq_size=st.one_of(st.integers(max_value=-1), st.floats(max_value=-0.1)))
     def test_invalid_seq_size(self, seq_size):
         cpp_plot = aa.CPPPlot()
@@ -474,7 +474,7 @@ class TestCCPlotFeatureMap:
             cpp_plot.feature_map(df_feat=df_feat, seq_size=seq_size)
         plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(fontsize_tmd_jmd=st.one_of(st.integers(max_value=-1), st.floats(max_value=-0.1)))
     def test_invalid_fontsize_tmd_jmd(self, fontsize_tmd_jmd):
         cpp_plot = aa.CPPPlot()
@@ -483,7 +483,7 @@ class TestCCPlotFeatureMap:
             cpp_plot.feature_map(df_feat=df_feat, fontsize_tmd_jmd=fontsize_tmd_jmd)
         plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(weight_tmd_jmd=st.sampled_from(INVALID_WEIGHT))
     def test_invalid_weight_tmd_jmd(self, weight_tmd_jmd):
         cpp_plot = aa.CPPPlot()
@@ -492,7 +492,7 @@ class TestCCPlotFeatureMap:
             cpp_plot.feature_map(df_feat=df_feat, weight_tmd_jmd=weight_tmd_jmd)
         plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(fontsize_labels=st.one_of(st.integers(max_value=-1), st.floats(max_value=-0.1)))
     def test_invalid_fontsize_labels(self, fontsize_labels):
         cpp_plot = aa.CPPPlot()
@@ -505,7 +505,7 @@ class TestCCPlotFeatureMap:
                                  fontsize_imp_bar=fontsize_labels)
         plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(add_xticks_pos=st.text(min_size=1))  # Invalid input for boolean parameter
     def test_invalid_add_xticks_pos(self, add_xticks_pos):
         cpp_plot = aa.CPPPlot()
@@ -515,7 +515,7 @@ class TestCCPlotFeatureMap:
         plt.close()
 
     # Negative tests: Legend, Axis, and Grid Configurations
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(grid_linewidth=st.floats(max_value=-0.01))  # Invalid negative grid_linewidth
     def test_invalid_grid_linewidth(self, grid_linewidth):
         cpp_plot = aa.CPPPlot()
@@ -524,7 +524,7 @@ class TestCCPlotFeatureMap:
             cpp_plot.feature_map(df_feat=df_feat, grid_linewidth=grid_linewidth)
         plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(grid_linecolor=st.sampled_from(INVALID_COLORS))  # Invalid grid_linecolor
     def test_invalid_grid_linecolor(self, grid_linecolor):
         cpp_plot = aa.CPPPlot()
@@ -533,7 +533,7 @@ class TestCCPlotFeatureMap:
             cpp_plot.feature_map(df_feat=df_feat, grid_linecolor=grid_linecolor)
         plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(border_linewidth=st.floats(max_value=-0.01))  # Invalid negative border_linewidth
     def test_invalid_border_linewidth(self, border_linewidth):
         cpp_plot = aa.CPPPlot()
@@ -542,7 +542,7 @@ class TestCCPlotFeatureMap:
             cpp_plot.feature_map(df_feat=df_feat, border_linewidth=border_linewidth)
         plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(facecolor_dark=st.text(min_size=1))  # Invalid input for optional boolean parameter
     def test_invalid_facecolor_dark(self, facecolor_dark):
         cpp_plot = aa.CPPPlot()
@@ -551,7 +551,7 @@ class TestCCPlotFeatureMap:
             cpp_plot.feature_map(df_feat=df_feat, facecolor_dark=facecolor_dark)
         plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(vmin=st.floats(min_value=10.0), vmax=st.floats(max_value=0.0))  # Invalid vmin > vmax
     def test_invalid_vmin_vmax(self, vmin, vmax):
         vmin, vmax = adjust_vmin_vmax(vmin=vmin, vmax=vmax)
@@ -562,7 +562,7 @@ class TestCCPlotFeatureMap:
                 cpp_plot.feature_map(df_feat=df_feat, vmin=vmin, vmax=vmax)
             plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(cmap=st.text(min_size=1))  # Invalid colormap name
     def test_invalid_cmap(self, cmap):
         if cmap not in VALID_COLORS:  # Adjust this condition based on your colormap validation logic
@@ -572,7 +572,7 @@ class TestCCPlotFeatureMap:
                 cpp_plot.feature_map(df_feat=df_feat, cmap=cmap)
             plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(cmap_n_colors=st.integers(max_value=0))  # Invalid non-positive cmap_n_colors
     def test_invalid_cmap_n_colors(self, cmap_n_colors):
         cpp_plot = aa.CPPPlot()
@@ -581,7 +581,7 @@ class TestCCPlotFeatureMap:
             cpp_plot.feature_map(df_feat=df_feat, cmap_n_colors=cmap_n_colors)
         plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(cbar_pct=st.text(min_size=1))  # Invalid input for boolean parameter
     def test_invalid_cbar_pct(self, cbar_pct):
         cpp_plot = aa.CPPPlot()
@@ -598,7 +598,7 @@ class TestCCPlotFeatureMap:
             cpp_plot.feature_map(df_feat=df_feat, cbar_kws=invalid_cbar_kws)
         plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(cbar_xywh=st.tuples(st.just(-0.1), st.just(-0.1), st.just(-0.1), st.just(-0.1)))  # Invalid cbar_xywh values
     def test_invalid_cbar_xywh(self, cbar_xywh):
         cpp_plot = aa.CPPPlot()
@@ -607,7 +607,7 @@ class TestCCPlotFeatureMap:
             cpp_plot.feature_map(df_feat=df_feat, cbar_xywh=cbar_xywh)
         plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(cbar_kws=st.text(min_size=1))  # Invalid input for dict parameter
     def test_invalid_cbar_kws(self, cbar_kws):
         cpp_plot = aa.CPPPlot()
@@ -616,7 +616,7 @@ class TestCCPlotFeatureMap:
             cpp_plot.feature_map(df_feat=df_feat, cbar_kws=cbar_kws)
         plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(dict_color=st.just({'invalid_cat': 'blue'}))
     def test_invalid_dict_color(self, dict_color):
         cpp_plot = aa.CPPPlot()
@@ -697,7 +697,7 @@ class TestCCPlotFeatureMap:
                 cpp_plot.feature_map(df_feat=df_feat, imp_bar_label_type=imp_bar_label_type)
             plt.close()
 
-    @settings(max_examples=3, deadline=5000)
+    @settings(max_examples=3, deadline=10000)
     @given(xtick_size=st.just(-1), xtick_width=st.just(-1), xtick_length=st.just(-1))
     def test_invalid_tick_styling(self, xtick_size, xtick_width, xtick_length):
         cpp_plot = aa.CPPPlot()
